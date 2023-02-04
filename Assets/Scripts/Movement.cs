4d36137597e3a69ec68eab175ea1634b254fda95
APBorK <1659732@gmail.com>
2023-02-04 09:53:19 +0300

    Create scene and movment

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0) || _startPosition!=transform.position)
+        {
+            Move();
+        }
+    }
+ 
+    void Move()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(myRay,out hitInfo))
+        {
+            transform.position = Vector3.Lerp(transform.position,
+                new Vector3(hitInfo.point.x,transform.position.y,hitInfo.point.z),Time.fixedDeltaTime * _speed);
+        }
+    }
+}
