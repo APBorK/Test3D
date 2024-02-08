ce30212ddac5a2b2703f3f4b958af97e65f2bfca
Citizen-Oleg <78354938+APBorK@users.noreply.github.com>
2024-02-08 14:50:30 +0300

    Update Bot.cs
    213

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -4,7 +4,7 @@ public class Bot : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-//123
+//123123
         if (collision.gameObject.CompareTag(Tags.Player))
         {
             EventSistem.SendKillBot();
